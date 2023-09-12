1a2b8883e7fc30fbdd0ff4fdeaf452722bf67f5c
Ian <44680390+IanC04@users.noreply.github.com>
2023-09-11 23:42:54 -0500

    1.4

--- Assignment 1/answers.m
+++ Assignment 1/answers.m
@@ -1,5 +1,8 @@
+clc, clearvars, close all;
+
 ut_image = imread("Assignment1_pics\ut.jpg");
 river_image = imread("Assignment1_pics\river.jpg");
+gsw_image = imread("Assignment1_pics\gsw.jpg");
 memory_image = imread("Assignment1_pics\persistence_of_memory.jpg");
 toucan_image = imread("Assignment1_pics\toucan.jpg");
 
@@ -7,16 +10,28 @@ toucan_image = imread("Assignment1_pics\toucan.jpg");
 % Question 1
 figure("Name", "Seam removal pictures")
 
-subplot(1, 3, 1);
+subplot(2, 3, 1);
 imshow(ut_image);
 title("Original")
 
-subplot(1, 3, 2);
+subplot(2, 3, 2);
 imshow(removeVertical(ut_image, 100));
 title("Removed 100 Vertical Seams");
 
-subplot(1, 3, 3);
+subplot(2, 3, 3);
 imshow(removeHorizontal(ut_image, 100));
+title("Removed 100 Horizontal Seams");
+
+subplot(2, 3, 4);
+imshow(river_image);
+title("Original")
+
+subplot(2, 3, 5);
+imshow(removeVertical(river_image, 100));
+title("Removed 100 Vertical Seams");
+
+subplot(2, 3, 6);
+imshow(removeHorizontal(river_image, 100));
 title("Removed 100 Horizontal Seams");
 %}
 
@@ -39,12 +54,6 @@ title("Cumulative Minimum Energy Function-Vertical");
 subplot(1, 4, 4);
 imagesc(createMinimumEnergyMatrix(createEnergyMatrix(ut_image), "Horizontal"));
 title("Cumulative Minimum Energy Function-Horizontal");
-
-% The reason for these outputs is that on top of each object there's a
-% triangle pointing to it, since for a object or background, the gradient
-% is small, but, when edges to objects are detected through large
-% gradients, the energy path will prioritize other paths which don't pass
-% through manny edges.
 %}
 
 %{
@@ -64,12 +73,6 @@ subplot(1, 3, 3);
 seam = getSeam(createEnergyMatrix(ut_image), "Horizontal");
 imshow(highlightSeam(ut_image, seam, "Horizontal"));
 title("Horizontal Seam");
-
-% These are the optimal seams since they include the blue sky background,
-% which is mostly the same color. Therefore, their cumulative gradient will
-% be small. However, in the vertical gradient, the trees in the foreground
-% cover the entire bottom half, so the path passed through the patch which
-% are mostly the same shade of the tree leaves.
 %}
 
 %{
@@ -91,32 +94,84 @@ subplot(1, 3, 3);
 seam = getSeam(createEnergyMatrix(ut_image), "Vertical");
 imshow(highlightSeam(ut_image, seam, "Vertical"));
 title("Vertical Seam of Blurred Image");
-
-% The results prove that blurring images first then calculating seams may
-% produce different results then calculating seams then blurring the image.
-% This is because the gradients of each pixel now are smaller since its
-% value is closer to its neighbors.
 %}
 
+%{
 % Question 5
-figure("Name", "Custom results")
+figure("Name", "Custom results");
 
 subplot(3, 4, 1);
-imshow(river_image);
-title("Original")
+imshow(gsw_image);
+title("Original(600×800×3)")
 
 subplot(3, 4, 2);
-picture = river_image;
+picture = gsw_image;
+picture = removeVertical(picture, 200);
+picture = removeHorizontal(picture, 200);
+imshow(picture);
+title("200 Vertical then 200 Horizontal Seam Resizing(400×600×3)");
+
+subplot(3, 4, 3);
+picture = gsw_image;
+[r, c] = size(picture, [1  2]);
+picture = imresize(picture, [r c] - 200, "nearest");
+imshow(picture);
+title("Nearest Neighbor Resizing(400×600×3)");
+
+subplot(3, 4, 4);
+picture = gsw_image;
+[r, c] = size(picture, [1  2]);
+picture = imresize(picture, [r c] - 200, "bicubic");
+imshow(picture);
+title("Bicubic Interpolation Resizing(400×600×3)");
+
+subplot(3, 4, 5);
+imshow(memory_image);
+title("Original(221×300×3)")
+
+subplot(3, 4, 6);
+picture = memory_image;
 picture = removeVertical(picture, 100);
 picture = removeHorizontal(picture, 100);
 imshow(picture);
-title("Seam Resizing of Image");
+title("100 Vertical then 100 Horizontal Seam Resizing(121×200×3)");
 
-subplot(3, 4, 3);
-picture = river_image;
+subplot(3, 4, 7);
+picture = memory_image;
 [r, c] = size(picture, [1  2]);
-picture = imresize(picture, [r c] - 100);
+picture = imresize(picture, [r c] - 100, "nearest");
 imshow(picture);
-title("MATLAB Resizing of Image");
+title("Nearest Neighbor Resizing(121×200×3)");
 
-% Original: 318×424×3   Resized: 218×324×3 uint8
+subplot(3, 4, 8);
+picture = memory_image;
+[r, c] = size(picture, [1  2]);
+picture = imresize(picture, [r c] - 100, "bicubic");
+imshow(picture);
+title("Bicubic Interpolation Resizing(121×200×3)");
+
+subplot(3, 4, 9);
+imshow(toucan_image);
+title("Original(419×640×3)")
+
+subplot(3, 4, 10);
+picture = toucan_image;
+picture = removeVertical(picture, 100);
+picture = removeHorizontal(picture, 100);
+imshow(picture);
+title("100 Vertical then 100 Horizontal Seam Resizing(319×540×3)");
+
+subplot(3, 4, 11);
+picture = toucan_image;
+[r, c] = size(picture, [1  2]);
+picture = imresize(picture, [r c] - 100, "nearest");
+imshow(picture);
+title("Nearest Neighbor Resizing(319×540×3)");
+
+subplot(3, 4, 12);
+picture = toucan_image;
+[r, c] = size(picture, [1  2]);
+picture = imresize(picture, [r c] - 100, "bicubic");
+imshow(picture);
+title("Bicubic Interpolation Resizing(319×540×3)");
+%}
