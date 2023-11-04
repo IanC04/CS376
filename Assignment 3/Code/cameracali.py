Need to fix essential matrix calculation

Assignment 3/Code/cameracali.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import keypoints
+import main
 
 
 def getPiMatrix(Coord2d: np.ndarray, Coord3d: np.ndarray) -> np.ndarray:
@@ -84,4 +85,10 @@ def calculate() -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
 
 
 if __name__ == "__main__":
-    calculate()
+    P, K, R, t = calculate()
+    main.manageMatrix(
+        ["Projection", "Intrinsic", f"Extrinsic Rotation with Determinant: {round(np.linalg.det(R), 5)}",
+         "Extrinsic Translation"],
+        [P, K, R, t],
+        save_result=True,
+        file_title="Part 1 Matrices")

Assignment 3/Code/relativepose.py
@@ -11,21 +11,46 @@ targetImg = cv2.cvtColor(targetImg, cv2.COLOR_BGR2RGB)
 
 
 def getRelativePose(E: np.ndarray) -> (np.ndarray, np.ndarray):
-    # TODO
-    return E, E
+    U, S, Vt = np.linalg.svd(E)
+    # TODO make sure S[0] == S[1] and S[2] == 0
+    T = S[0] * U[:, 2]
+    T = T.reshape((3, 1))
+    R = np.array([-U[:, 1], U[:, 0], U[:, 2]]) @ Vt
+    if np.linalg.det(R) < 0:
+        R = -R
+        T = -T
+    return R, T
 
 
 def estimateEssentialMatrix(matrices: (np.ndarray, np.ndarray, np.ndarray, np.ndarray), correspondences: np.ndarray,
                             ptSource: np.ndarray, ptTarget: np.ndarray) -> (
         np.ndarray):
+    E = np.zeros((3, 3))
     P = matrices[0]
     K = matrices[1]
-    threeD_Source = P @ ptSource
-    threeD_Target = P @ ptTarget
-    x1 = correspondences[:, 0]
-    # TODO use K matrix(inverse?) to get 3d coordinates then get smallest eigenvector corresponding to smallest
-    #  eigenvalue
-    return correspondences
+    # TODO: fix (X, Y, Z) = K^-1 (x, y, 1)
+    K_1 = np.linalg.inv(K)
+    twoD_Source = np.column_stack((ptSource, np.ones(ptSource.shape[0]))).T
+    twoD_Target = np.column_stack((ptTarget, np.ones(ptTarget.shape[0]))).T
+    threeD_Source = K_1 @ twoD_Source
+    threeD_Target = K_1 @ twoD_Target
+    # Get smallest eigenvector corresponding to smallest eigenvalue
+
+    A = np.zeros(shape=(correspondences.shape[0], 9))
+
+    for i in range(correspondences.shape[0]):
+        source = threeD_Source[:, i]
+        target = threeD_Target[:, i]
+        x1, y1, z1 = source[0], source[1], source[2]
+        x2, y2, z2 = target[0], target[1], target[2]
+        a = np.array([x1 * x2, y1 * x2, z1 * x2, x1 * y2, y1 * y2, z1 * y2, x1 * z2, y1 * z2, z1 * z2]).T
+        A[i, :] = a
+
+    A = A.T @ A
+    eigenvalues, eigenvectors = np.linalg.eig(A)
+    smallest_eigenvector = eigenvectors[:, -1]
+    E = smallest_eigenvector.reshape((3, 3))
+    return E
 
 
 def getCorrespondences() -> (np.ndarray, np.ndarray, np.ndarray):
@@ -42,7 +67,7 @@ def getCorrespondences() -> (np.ndarray, np.ndarray, np.ndarray):
     matches = np.array(matches)
 
     # Top 10 matches
-    matches = matches[:10]
+    matches = matches[:8]
 
     ptS = list()
     ptT = list()
@@ -54,19 +79,25 @@ def getCorrespondences() -> (np.ndarray, np.ndarray, np.ndarray):
     ptT = np.array(ptT)
 
     img_matches = cv2.drawMatches(sourceImg, kpS, targetImg, kpT, matches, None,
-                                  flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS | cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+                                  flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS |
+                                        cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     main.manageImage(["Matches-SIFT"], [img_matches], display_result=False,
                      save_result=False, file_title="Matches-SIFT")
     return matches, ptS, ptT
 
 
-def calculate() -> (np.ndarray, np.ndarray):
+def calculate() -> (np.ndarray, np.ndarray, np.ndarray):
     P, K, R, t = cameracali.calculate()
     correspondences, ptS, ptT = getCorrespondences()
     E = estimateEssentialMatrix((P, K, R, t), correspondences, ptS, ptT)
     R, T = getRelativePose(E)
-    return R, T
+    return E, R, T
 
 
 if __name__ == "__main__":
-    calculate()
+    E, R, T = calculate()
+    main.manageMatrix(
+        ["Essential", f"Rotation with Determinant: {round(np.linalg.det(R), 5)}", "Translation"],
+        [E, R, T],
+        save_result=True,
+        file_title="Part 2 Matrices")