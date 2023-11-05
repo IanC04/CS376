75fca81e6fbf5175c9a57090b4c90b4995966386
Ian <44680390+IanC04@users.noreply.github.com>
2023-11-04 23:18:51 -0500

    Add different configurations

--- Assignment 3/Code/keypoints.py
+++ Assignment 3/Code/keypoints.py
@@ -4,6 +4,8 @@ import numpy as np
 calibrationImg = cv2.imread("../Assignment 3 Pics/Calibration.jpg")
 calibrationImg = cv2.cvtColor(calibrationImg, cv2.COLOR_BGR2RGB)
 NEON_GREEN = (57, 255, 0)
+NEON_BLUE = (31, 81, 255)
+NEON_RED = (255, 49, 49)
 
 
 def get2D(img: np.ndarray) -> np.ndarray:
@@ -40,6 +42,36 @@ def calculate(img: np.ndarray = calibrationImg) -> (np.ndarray, np.ndarray):
     two_d = get2D(img)
     three_d = get3D(img)
     return two_d, three_d
+
+
+def tryConfiguration(i: int) -> (np.ndarray, np.ndarray):
+    match i:
+        case 0:
+            return (np.array([[1971, 1377], [1441, 1558], [1706, 1559], [1971, 1555], [2236, 1553], [2499, 1553],
+                              [1149, 1762], [2765, 1742], [1504, 2112], [1975, 2750]], dtype=np.int64).T,
+                    np.array([[6, 6, 0], [5, 1, 0], [4, 2, 0], [3, 3, 0], [2, 4, 0], [1, 5, 0],
+                              [6, 0, 1], [0, 6, 1], [3, 0, 2], [0, 0, 4]], dtype=np.int64).T)
+
+        case 1:
+            return (np.array([[1971, 1377], [1441, 1558], [1706, 1559], [1824, 1685], [2234, 1554], [2765, 1742]],
+                             dtype=np.int64).T,
+                    np.array([[6, 6, 0], [5, 1, 0], [4, 2, 0], [2, 1, 0], [1, 4, 0], [0, 6, 1]], dtype=np.int64).T)
+
+        case 2:
+            return (np.array([[1971, 1866], [1804, 1806], [1975, 1735], [2139, 1791], [2142, 2016], [1781, 2040]],
+                             dtype=np.int64).T,
+                    np.array([[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0], [0, 1, 1], [1, 0, 1]], dtype=np.int64).T)
+
+        case 3:
+            return (np.array([[962, 2010], [1969, 1862], [1974, 2748], [2935, 1981], [2863, 1526], [2160, 1293]],
+                             dtype=np.int64).T,
+                    np.array([[8, 0, 3], [0, 0, 0], [0, 0, 4], [0, 8, 3], [0, 7, 0], [7, 9, 0]], dtype=np.int64).T)
+
+        case 4:
+            return (np.array([[1972, 1425], [2650, 2146], [1250, 1993], [1975, 2543], [1970, 1863]],
+                             dtype=np.int64).T,
+                    np.array([[5, 5, 0], [0, 5, 3], [5, 0, 2], [0, 0, 3], [0, 0, 0]], dtype=np.int64).T)
+    pass
 
 
 if __name__ == "__main__":
