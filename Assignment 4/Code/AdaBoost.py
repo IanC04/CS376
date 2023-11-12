96ddba7c3130621780334f5a35649bbea797bf36
Ian <44680390+IanC04@users.noreply.github.com>
2023-11-12 00:43:23 -0600

    Adaboost v0.1

--- Assignment 4/Code/AdaBoost.py
+++ Assignment 4/Code/AdaBoost.py
@@ -2,12 +2,19 @@ import LoadImages
 import numpy as np
 import matplotlib.pyplot as plt
 
+def classify():
+    pass
 
 def train():
     pass
 
+def test():
+    pass
 
+classifier = None
 # Uses weak classifiers to classify images
 if __name__ == "__main__":
     print("AdaBoost.py")
     training, testing, labels = LoadImages.all_images()
+    classifier = np.zeros((labels[b'num_vis'], 2))
+    pass
