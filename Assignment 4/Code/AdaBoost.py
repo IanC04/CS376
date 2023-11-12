235764cc590256042a25d739035b0ae1a09d6599
Ian <44680390+IanC04@users.noreply.github.com>
2023-11-12 00:04:01 -0600

    Adaboost skeleton

--- Assignment 4/Code/AdaBoost.py
+++ Assignment 4/Code/AdaBoost.py
@@ -1,1 +1,13 @@
+import LoadImages
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def train():
+    pass
+
+
 # Uses weak classifiers to classify images
+if __name__ == "__main__":
+    print("AdaBoost.py")
+    training, testing, labels = LoadImages.all_images()
