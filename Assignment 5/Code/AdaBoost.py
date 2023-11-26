9370b0e9937b21ab30e33a9604f27195c8c566b5
Ian <44680390+IanC04@users.noreply.github.com>
2023-11-25 21:46:50 -0600

    AdaBoost Skeleton

---
+++ Assignment 5/Code/AdaBoost.py
@@ -0,0 +1,18 @@
+"""
+Written by Ian Chen on 11/25/2023
+GitHub: https://github.com/IanC04
+"""
+
+
+class AdaBoost:
+    """
+    AdaBoost class
+    """
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
