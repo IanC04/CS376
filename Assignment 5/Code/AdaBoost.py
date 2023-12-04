b7d7dd62daaebbfbf616fefcb43adbdb669c077c
Ian <44680390+IanC04@users.noreply.github.com>
2023-12-04 00:41:29 -0600

    Compute haar features

--- Assignment 5/Code/AdaBoost.py
+++ Assignment 5/Code/AdaBoost.py
@@ -11,6 +11,18 @@ from tqdm import tqdm
 import LoadImages
 
 
+def load():
+    """
+    Load the model
+    """
+    import pickle
+    with open(f"{LoadImages.CACHE_PATH}/model.pkl", "rb") as f:
+        print("Loading model...")
+        m = pickle.load(f)
+    del pickle
+    return m
+
+
 class AdaBoost:
     """
     AdaBoost class
@@ -20,8 +32,10 @@ class AdaBoost:
                  window_size=(24, 24)):
         """
         Initialize the AdaBoost class
-        :param folds: array of folds in the form: (x1, y1, x2, y2)
-        :param images:
+        :param training_folds: array of folds in the form: (x1, y1, x2, y2)
+        :param testing_folds: array of folds in the form: (x1, y1, x2, y2)
+        :param training_images:
+        :param testing_images:
         :param window_size:
         """
         self.original_training_folds = training_folds
@@ -30,62 +44,89 @@ class AdaBoost:
         self.original_testing_images = testing_images
         self.window_size = window_size
         self.haar_indices = self.generate_haar_indicies(self.window_size)
+        self.haar_indices_subset = self.generate_haar_indices_subset(self.haar_indices)
+
         """
-        Requires modification of original data to have non-face images
+        Requires modification of original data to have non-face images, windows where no face is 
+        shown
         Inspired by https://citeseerx.ist.psu.edu/document?repid=rep1&type=pdf&doi
         =d0ea54f1a97a1922c679f88c6167328da472a259
         """
         self.training_faces, self.training_non_faces = self.generate_gray_and_scaled_faces(
-            self.original_training_folds, self.original_training_images, self.window_size)
+            "training", self.original_training_folds, self.original_training_images,
+            self.window_size)
         self.testing_faces, self.testing_non_faces = self.generate_gray_and_scaled_faces(
-            self.original_testing_folds, self.original_testing_images, self.window_size)
+            "testing", self.original_testing_folds, self.original_testing_images, self.window_size)
 
-        self.training_face_iimages = self.generate_integral_images("training_faces",
-                                                                   self.training_faces)
-        self.training_non_face_iimages = self.generate_integral_images("training_non_faces",
-                                                                       self.training_non_faces)
-        self.testing_face_iimages = self.generate_integral_images("testing_faces",
-                                                                  self.testing_faces)
-        self.testing_non_face_iimages = self.generate_integral_images("testing_non_faces",
-                                                                      self.testing_non_faces)
+        self.training_face_iimages, self.training_non_face_iimages = self.generate_integral_images(
+            "training",
+            self.training_faces, self.training_non_faces)
+        self.testing_face_iimages, self.testing_non_face_iimages = self.generate_integral_images(
+            "testing",
+            self.testing_faces, self.testing_non_faces)
+
+        self.training_face_haar_features, self.training_non_face_haar_features = (
+            self.get_haar_features("training", self.haar_indices_subset, self.training_face_iimages,
+                                   self.training_non_face_iimages))
+
+        self.testing_face_haar_features, self.testing_non_face_haar_features = (
+            self.get_haar_features("testing", self.haar_indices_subset, self.testing_face_iimages,
+                                   self.testing_non_face_iimages))
         pass
 
     @classmethod
-    def generate_gray_and_scaled_faces(cls, folds, images, window_size: tuple) -> (
+    def generate_gray_and_scaled_faces(cls, file_name, folds, images, window_size: tuple) -> (
             np.ndarray, np.ndarray):
         """
         Generate faces from the folds and images
+        :param file_name:
         :param folds:
         :param images:
+        :param window_size:
         :return:
         """
-        all_faces = list()
-        all_non_faces = list()
-        for index in tqdm(range(len(folds)), desc="Generating faces"):
-            for img_name in images[index].keys():
-                img = cls.rgb2gray(images[index][img_name])
-                print(img_name)
-                if img_name == "2002/07/19/big/img_581":
-                    pass
-                faces, non_faces = cls.get_faces(folds[index][img_name], img, window_size)
-                all_faces.extend(faces)
-                all_non_faces.extend(non_faces)
+        if os.path.exists(f"{LoadImages.CACHE_PATH}/{file_name}_faces_and_non_faces.npy"):
+            with open(f"{LoadImages.CACHE_PATH}/{file_name}_faces_and_non_faces.npy", "rb") as f:
+                print("Cache found. Loading faces...")
+                all_faces = np.load(f, allow_pickle=True)
+                print("Cache found. Loading non faces...")
+                all_non_faces = np.load(f, allow_pickle=True)
+            return all_faces, all_non_faces
+        else:
+            print("Cache not found. Generating faces...")
+            all_faces = list()
+            all_non_faces = list()
+            for index in tqdm(range(len(folds)), desc="Generating faces"):
+                for img_name in images[index].keys():
+                    img = cls.rgb2gray(images[index][img_name])
+                    print(img_name)
+                    faces, non_faces = cls.get_faces_and_non_faces(folds[index][img_name], img,
+                                                                   window_size)
+                    all_faces.extend(faces)
+                    all_non_faces.extend(non_faces)
 
-        all_faces = np.array(all_faces)
-        all_non_faces = np.array(all_non_faces)
-        return all_faces, all_non_faces
+            all_faces = np.array(all_faces)
+            all_non_faces = np.array(all_non_faces)
+            with open(f"{LoadImages.CACHE_PATH}/{file_name}_faces_and_non_faces.npy", "wb") as f:
+                np.save(f, all_faces)
+                np.save(f, all_non_faces)
+            return all_faces, all_non_faces
 
     @classmethod
-    def get_faces(cls, folds, img, window_size: tuple):
+    def get_faces_and_non_faces(cls, folds, img, window_size: tuple):
         """
         Get the face from the fold and image
-        :param fold:
+        Potentially normalize the results to have unit variance
+        :param folds:
         :param img:
+        :param window_size:
         :return:
         """
         temp_img = img.copy()
         faces, non_faces = list(), list()
         face_coord = None
+
+        # print(f"Number of faces: {len(folds)}")
         for i, fold in enumerate(folds):
             # Convert [x1, y1, x2, y2] to int([x1, y1, x2, y2])
             x1, y1, x2, y2 = int(fold[0]), int(fold[1]), int(fold[2]), int(fold[3])
@@ -99,6 +140,9 @@ class AdaBoost:
             face_coord = np.array([x1, y1, x2, y2])
 
             # See if the face is correct
+            # cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 1)
+            # plt.imshow(img, cmap="gray")
+            # plt.waitforbuttonpress()
             # plt.imshow(faces[i], cmap="gray")
             # plt.waitforbuttonpress()
 
@@ -108,29 +152,27 @@ class AdaBoost:
         return faces, non_faces
 
     @classmethod
-    def get_non_faces(cls, img, window_size: tuple, face) -> list:
+    def get_non_faces(cls, img: np.ndarray, window_size: tuple, face: np.ndarray) -> list:
         """
-        Get the non faces from the fold and image, get windows where minimal face is shown
-        :param fold:
+        Get the non faces from the image, get windows where minimal face is shown
         :param img:
         :param window_size:
-        :param face: [x1, y1, x2, y2]
+        :param face: [X1, y1, x2, y2]
         :return:
         """
         # Check if face blacked out
-        plt.imshow(img, cmap="gray")
-        plt.waitforbuttonpress()
         non_faces = list()
 
-        scale_x, scale_y = window_size[0] * 2 / img.shape[0], window_size[1] * 2 / img.shape[1]
+        # Flipped to scale down
+        scale_x, scale_y = window_size[0] * 2 / img.shape[1], window_size[1] * 2 / img.shape[0]
         img = cv2.resize(img, (window_size[0] * 2, window_size[1] * 2))
 
         # Rounding issue?
         f_x1, f_y1, f_x2, f_y2 = np.round(face * [scale_x, scale_y, scale_x, scale_y]).astype(int)
 
-        cv2.rectangle(img, (f_x1, f_y1), (f_x2, f_y2), (255, 0, 0), 2)
-        plt.imshow(img, cmap="gray")
-        plt.waitforbuttonpress()
+        # cv2.rectangle(img, (f_x1, f_y1), (f_x2, f_y2), (255, 0, 0), 1)
+        # plt.imshow(img, cmap="gray")
+        # plt.waitforbuttonpress()
         for x in range(0, 2):
             for y in range(0, 2):
                 if face is not None:
@@ -140,11 +182,11 @@ class AdaBoost:
                     width = xi2 - xi1
                     height = yi2 - yi1
                     face_percentage = width * height / (window_size[0] * window_size[1])
-                    print(f"Face Percentage: {face_percentage}, Width: {width}, Height: {height}")
+                    # print(f"Face Percentage: {face_percentage}, Width: {width}, Height: {height}")
                     if (width > 0 and height > 0) and face_percentage > 0.1:
                         continue
 
-                # TODO: Non face is sections where not face is shown
+                # Non-face is sections where not face is shown
                 non_face = img[y * window_size[1]:y * window_size[1] + window_size[1],
                            x * window_size[0]:x * window_size[0] + window_size[0]]
                 non_faces.append(non_face)
@@ -184,34 +226,31 @@ class AdaBoost:
             pickle.dump(self, f)
         del pickle
 
-    def load(self):
-        """
-        Load the model
-        """
-        import pickle
-        with open(f"{LoadImages.CACHE_PATH}/model.pkl", "rb") as f:
-            print("Loading model...")
-            model = pickle.load(f)
-        del pickle
-        return model
-
     @classmethod
-    def generate_integral_images(cls, file_name, imgs: np.ndarray) -> np.ndarray:
+    def generate_integral_images(cls, file_name, face_imgs: np.ndarray,
+                                 non_face_imgs: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         """
         Generate integral images from images
-        :param imgs:
+        :param file_name:
+        :param face_imgs:
+        :param non_face_imgs:
         :return:
         """
         if os.path.exists(f"{LoadImages.CACHE_PATH}/{file_name}_integral_images.npy"):
             with open(f"{LoadImages.CACHE_PATH}/{file_name}_integral_images.npy", "rb") as f:
                 print("Cache found. Loading integral images...")
-                integral_images = np.load(f, allow_pickle=True)
+                face_integral_images = np.load(f, allow_pickle=True)
+                non_face_integral_images = np.load(f, allow_pickle=True)
+                return face_integral_images, non_face_integral_images
         else:
             print("Cache not found. Generating integral images...")
-            integral_images = cls.convert_to_integral(imgs)
+            face_integral_images = cls.convert_to_integral(face_imgs)
+            non_face_integral_images = cls.convert_to_integral(non_face_imgs)
+
             with open(f"{LoadImages.CACHE_PATH}/{file_name}_integral_images.npy", "wb") as f:
-                np.save(f, integral_images)
-        return integral_images
+                np.save(f, face_integral_images)
+                np.save(f, non_face_integral_images)
+        return face_integral_images, non_face_integral_images
 
     @classmethod
     def convert_to_integral(cls, imgs: np.ndarray) -> np.ndarray:
@@ -220,13 +259,11 @@ class AdaBoost:
         :param imgs:
         :return:
         """
-        integral_imgs = list()
+        integral_imgs = np.zeros(imgs.shape)
         for index in tqdm(range(len(imgs)), desc="Converting to integral images"):
-            i_imgs = dict()
-            for img_name in imgs[index].keys():
-                i_imgs[img_name] = cls.get_integral_image(imgs[index][img_name])
-            integral_imgs.append(i_imgs)
-        integral_imgs = np.array(integral_imgs)
+            i_imgs = cls.get_integral_image(imgs[index])
+            integral_imgs[index] = i_imgs
+        # integral_imgs = np.array(integral_imgs)
         return integral_imgs
 
     @classmethod
@@ -242,11 +279,11 @@ class AdaBoost:
     def get_integral_image(cls, img: np.ndarray) -> np.ndarray:
         """
         Get the integral image from an image
-        :param img:
+        :param img: GRAYSCALE image that should already be scaled to the window size
         :return:
         """
-        assert img.shape[2] == 3, f"Image shape is not 3: {img.shape}"
-        img = cls.rgb2gray(img)
+        assert img.ndim == 2 and img.shape[0] == img.shape[1], f"Image shape is not 2: {img.shape}"
+        # img = cls.rgb2gray(img)
         integral_img = np.cumsum(np.cumsum(img, axis=0), axis=1)
         return integral_img
 
@@ -259,8 +296,8 @@ class AdaBoost:
         two_piece = []
         three_piece = []
         four_piece = []
-        if os.path.isfile(f"{LoadImages.CACHE_PATH}/haar_indices.npy"):
-            with open(f"{LoadImages.CACHE_PATH}/haar_indices.npy", 'rb') as f:
+        if os.path.isfile(f"{LoadImages.CACHE_PATH}/all_haar_indices.npy"):
+            with open(f"{LoadImages.CACHE_PATH}/all_haar_indices.npy", 'rb') as f:
                 print("Cache found. Loading Haar Indices...")
                 two_piece = np.load(f)
                 three_piece = np.load(f)
@@ -269,8 +306,8 @@ class AdaBoost:
             width, height = size
             for x in tqdm(range(width), desc="Getting Haar Indices"):
                 for y in range(height):
-                    for w in range(1, width):
-                        for h in range(1, height):
+                    for w in range(1, width + 1):
+                        for h in range(1, height + 1):
                             if y + h - 1 < height and x + 2 * w - 1 < width:
                                 # Horizontal-2
                                 two_piece.append(
@@ -303,12 +340,94 @@ class AdaBoost:
             two_piece = np.array(two_piece)
             three_piece = np.array(three_piece)
             four_piece = np.array(four_piece)
-            with open(f"{LoadImages.CACHE_PATH}/haar_indices.npy", 'wb') as f:
+            with open(f"{LoadImages.CACHE_PATH}/all_haar_indices.npy", 'wb') as f:
                 np.save(f, two_piece)
                 np.save(f, three_piece)
                 np.save(f, four_piece)
                 print("Saved Haar Indices to Cache")
         return two_piece, three_piece, four_piece
+
+    @classmethod
+    def generate_haar_indices_subset(cls, haar_indices: tuple[np.ndarray, np.ndarray, np.ndarray],
+                                     subset_size=5000) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Generate a subset of the haar indices, ideally with substantial rectangle sizes
+        :param haar_indices:
+        :param subset_size:
+        :return:
+        """
+        if os.path.isfile(f"{LoadImages.CACHE_PATH}/haar_indices_subset.npy"):
+            with open(f"{LoadImages.CACHE_PATH}/haar_indices_subset.npy", 'rb') as f:
+                print("Cache found. Loading Haar Indices Subset...")
+                two_piece_subset = np.load(f)
+                three_piece_subset = np.load(f)
+                four_piece_subset = np.load(f)
+                return two_piece_subset, three_piece_subset, four_piece_subset
+        else:
+            print("Cache not found. Generating Haar Indices Subset...")
+            two_piece, three_piece, four_piece = haar_indices
+
+            generator = np.random.default_rng()
+            partition = generator.integers(subset_size, size=2)
+            partition.sort()
+            two_piece_subset = generator.choice(two_piece, partition[0], replace=False)
+            three_piece_subset = generator.choice(three_piece, partition[1] - partition[0],
+                                                  replace=False)
+            four_piece_subset = generator.choice(four_piece, subset_size - partition[1],
+                                                 replace=False)
+            with open(f"{LoadImages.CACHE_PATH}/haar_indices_subset.npy", 'wb') as f:
+                np.save(f, two_piece_subset)
+                np.save(f, three_piece_subset)
+                np.save(f, four_piece_subset)
+                print("Saved Haar Indices Subset to Cache")
+            return two_piece_subset, three_piece_subset, four_piece_subset
+
+    @classmethod
+    def display_haar_rectangles(cls, img, features):
+        """
+        Display the haar rectangles, currently only supports 2 piece haar features
+        :param img:
+        :param features:
+        :return:
+        """
+        for i in features:
+            random_image = img.copy()
+            cv2.rectangle(random_image, (i[1], i[0]), (i[3], i[2]), (255, 0, 0), 1)
+            cv2.rectangle(random_image, (i[5], i[4]), (i[7], i[6]), (0, 255, 0), 1)
+            plt.imshow(random_image, cmap="gray")
+            plt.waitforbuttonpress()
+
+    @classmethod
+    def get_haar_features(cls, file_name, haar_features, face_iimages, non_face_iimages) -> tuple[
+        np.ndarray, np.ndarray]:
+        """
+        Get the haar features from the images
+        :param file_name:
+        :param haar_features:
+        :param face_iimages:
+        :param non_face_iimages:
+        :return:
+        """
+
+        if os.path.exists(f"{LoadImages.CACHE_PATH}/{file_name}_haar_features.npy"):
+            with open(f"{LoadImages.CACHE_PATH}/{file_name}_haar_features.npy", "rb") as f:
+                print("Cache found. Loading Haar Features...")
+                face_haar_features = np.load(f, allow_pickle=True)
+                non_face_haar_features = np.load(f, allow_pickle=True)
+                return face_haar_features, non_face_haar_features
+        else:
+            print("Cache not found. Generating Haar Features...")
+
+            for i in tqdm(range(len(haar_features)), desc="Getting Haar Features"):
+                # TODO
+                face_haar_features = cls.get_haar_features_helper(haar_features, face_iimages)
+                non_face_haar_features = cls.get_haar_features_helper(haar_features, non_face_iimages)
+
+            with open(f"{LoadImages.CACHE_PATH}/{file_name}_haar_features.npy", "wb") as f:
+                np.save(f, face_haar_features)
+                np.save(f, non_face_haar_features)
+                print("Saved Haar Features to Cache")
+            return face_haar_features, non_face_haar_features
 
 
 if __name__ == "__main__":
